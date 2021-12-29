744ae01e1b4d71e731aad06c335efdf129aac22a
boardguy1024 <boardguy1024@gmail.com>
2021-12-29 21:58:17 +0900

    first commit

---
+++ SwiftUI_MagnificationGestureAnim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_MagnificationGestureAnim
+//
+//  Created by park kyung seok on 2021/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_MagnificationGestureAnim/SwiftUI_MagnificationGestureAnimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MagnificationGestureAnimApp.swift
+//  SwiftUI_MagnificationGestureAnim
+//
+//  Created by park kyung seok on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MagnificationGestureAnimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
